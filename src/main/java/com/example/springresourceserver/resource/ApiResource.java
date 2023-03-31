f6f72256b6a2750137183d7ae8d8c92d632774ae
John Adeshola <adla_jadeshola@idcontractors.net>
2023-03-31 10:30:18 +0100

    refactoring

--- src/main/java/com/example/springresourceserver/resource/ApiResource.java
+++ src/main/java/com/example/springresourceserver/resource/ApiResource.java
@@ -1,4 +1,4 @@
-package com.example.springresourceserver;
+package com.example.springresourceserver.resource;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
