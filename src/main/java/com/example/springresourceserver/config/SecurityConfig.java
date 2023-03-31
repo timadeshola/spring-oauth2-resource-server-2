bb20c1dd519f70242ae77cb244f86ce3514792e5
John Adeshola <adla_jadeshola@idcontractors.net>
2023-03-31 10:13:56 +0100

    update issuer uri

--- src/main/java/com/example/springresourceserver/config/SecurityConfig.java
+++ src/main/java/com/example/springresourceserver/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.springresourceserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,14 +18,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
+    private String issuerUri;
 
     @Bean
-    @Order(1)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuer))))
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt((jwt) -> jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))))
                 .build();
     }
 }
