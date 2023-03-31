f9fec237ff6c5af978ad0ad92b3e97a048f45680
John Adeshola <adla_jadeshola@idcontractors.net>
2023-03-31 09:38:17 +0100

    first commit

---
+++ src/main/java/com/example/springresourceserver/ApiResource.java
@@ -0,0 +1,21 @@
+package com.example.springresourceserver;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Project title: spring-resource-server
+ *
+ * @author johnadeshola
+ * Date: 3/31/23
+ * Time: 9:25 AM
+ */
+@RestController
+public class ApiResource {
+
+    @GetMapping("test")
+    public String home(Authentication auth) {
+        return "Welcome Home! " + auth.getName();
+    }
+}
---
+++ src/test/java/com/example/springresourceserver/SpringResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
